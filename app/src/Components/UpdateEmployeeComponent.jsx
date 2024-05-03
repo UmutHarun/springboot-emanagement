d163db4d5bd06f9359fa9a2e7022cfad6163eb48
UmutHarunJS <harun123ors@gmail.com>
2024-05-03 23:59:17 +0300

    form-valid added

--- app/src/Components/UpdateEmployeeComponent.jsx
+++ app/src/Components/UpdateEmployeeComponent.jsx
@@ -1,6 +1,6 @@
-import React, { useState , useEffect,useHistory} from 'react'
+import React, { useState , useEffect} from 'react'
 import EmployeeService from '../Services/EmployeeService';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams , useNavigate } from 'react-router-dom';
 
 function UpdateEmployeeComponent() {
     const { id } = useParams();
@@ -10,7 +10,8 @@ function UpdateEmployeeComponent() {
         lastName: '',
         emailId: ''
     });
-    const history = useHistory();
+    const [validValues,setValidValues] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         EmployeeService.getEmployeeById(id).then(res => {
@@ -27,13 +28,19 @@ function UpdateEmployeeComponent() {
     const updateEmployee = (e) => {
         e.preventDefault();
         EmployeeService.updateEmployee(id, employee).then(res => {
-            history.push('/employees');
+            navigate('/employees');
         });
     };
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setEmployee({ ...employee, [name]: value });
+        if(value.length === 0){
+            setValidValues(true);
+        }
+        else{
+            setValidValues(false);
+        }
     };
 
     return (
@@ -41,18 +48,18 @@ function UpdateEmployeeComponent() {
             <h3 className="text-center mt-4">Update Employee</h3>
             <form>
                 <div className="form-group py-3">
-                    <label className="d-flex justify-content-start"> First Name: </label>
-                    <input placeholder="First Name" name="firstName" className="form-control" 
+                    <label className={`d-flex justify-content-start ${validValues ? 'text-danger' : 'text-muted'}`}> First Name: </label>
+                    <input placeholder="First Name" name="firstName" className={`form-control ${validValues ? 'bg-danger' : 'bg-white'}`} 
                         value={employee.firstName} onChange={handleInputChange} />
                 </div>
                 <div className="form-group py-3">
-                    <label className="d-flex justify-content-start"> Last Name: </label>
-                    <input placeholder="Last Name" name="lastName" className="form-control" 
+                    <label className={`d-flex justify-content-start ${validValues ? 'text-danger' : 'text-muted'}`}> Last Name: </label>
+                    <input placeholder="Last Name" name="lastName" className={`form-control ${validValues ? 'bg-danger' : 'bg-white'}`} 
                         value={employee.lastName} onChange={handleInputChange} />
                 </div>
                 <div className="form-group py-3">
-                    <label className="d-flex justify-content-start"> Email Address: </label>
-                    <input placeholder="Email Address" name="emailId" className="form-control" 
+                    <label className={`d-flex justify-content-start ${validValues ? 'text-danger' : 'text-muted'}`}> Email Address: </label>
+                    <input placeholder="Email Address" name="emailId" className={`form-control ${validValues ? 'bg-danger' : 'bg-white'}`} 
                         value={employee.emailId} onChange={handleInputChange} />
                 </div>
                 <div className="py-3">
